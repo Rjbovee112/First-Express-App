f6e2d2497360bd6c84db6539a3d3ab47c47c119c
Robert Bovee <Rjbovee112@gmail.com>
2020-10-05 21:11:35 -0600

    adding small things

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ app.listen(3000, function () {
     console.log("Server Has Started");
 });
 // "/bye" => "Goodbye"
+app.get("/bye", function (req, res) {
+    res.send("Goodbye");
+});
 // "/dog" => "MEOW!"
+app.get("/dog", function (req, res) {
+    res.send("MEOW!");
+})
