38de5ed6f0ba5675a30568517749888af24450b5
Robert Bovee <Rjbovee112@gmail.com>
2020-10-05 21:01:41 -0600

    adding hi there test/connection

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var express = require("express");
 var app = express();
 
+// "/" => "Hi there!"
+app.get("/", function (req, res) {
+    res.send("Hi there!");
+})
+app.listen(3000, function () {
+    console.log("Server Has Started");
+});
+// "/bye" => "Goodbye"
+// "/dog" => "MEOW!"
